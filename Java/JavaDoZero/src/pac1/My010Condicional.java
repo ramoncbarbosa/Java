ba7e6f33438f9ff9c58996715519f33d5da19615
Ramon C. Barbosa <ramon.cobarbosa@gmai.com>
2022-10-23 14:09:53 -0300

    JavaInic

---
+++ Java/JavaDoZero/src/pac1/My010Condicional.java
@@ -0,0 +1,21 @@
+package pac1;
+
+import java.util.Scanner;
+
+public class My010Condicional {
+    public static void main(String[] args){
+        Scanner output = new Scanner(System.in);
+        float n1 = output.nextFloat();
+        float n2 = output.nextFloat();
+        float m = (n1 + n2)/2;
+        System.out.println(m);
+
+        if (m>9){
+            System.out.println("Parabéns");
+        } else if (m<=4.9) {
+            System.out.println("Lamento carinha");
+        } else {
+            System.out.println("Legal");
+        }
+    }
+}
